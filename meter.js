993da8f46dde78b16d6776cd005c7a2dc6b6dd7f
VeenaPDas96 <66079281+VeenaPDas96@users.noreply.github.com>
2020-06-04 16:18:49 +0530

    Update and rename script.js to meter.js

--- meter.js
+++ meter.js
@@ -15,7 +15,7 @@ var svg = d3.select(".completion-chart").append("svg")
     .attr("width", width)
     .attr("height", height)
 
-    .attr('fill', '#2E7AF9')
+    .attr('fill', '#0c44a1')
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
@@ -37,7 +37,7 @@ var text2 = meter.append("text")
     .attr("text-anchor", "middle")
     .attr("class", "text2");
 
-text2.text('progress completed');
+text2.text('Progress Completed');
 
 var animate = function(percentage){
     var i = d3.interpolate(progress, percentage);
@@ -50,11 +50,33 @@ var animate = function(percentage){
         };
     });
 }; 
-
 setTimeout(function () {
   animate($('#inputVal').val());
 }, 500);
-
+$.getJSON('data.json', function(data) {
+       var myItems = data.info;
+        $('.name').html(myItems[0].name);
+        $('.ecode').html(myItems[0].ecode);
+        $('.etype').html(myItems[0].Employee_Type);
+        $('.mlevel').html(myItems[0].Managment_Level);
+        $('.timetype').html(myItems[0].Time_Type);
+        $('.location').html(myItems[0].Location);
+        $('#inputVal').val(myItems[0].Progress);
+});
+$('.front-stars').css('width',$('.inputBx').val()*100+"%");
+if($('.inputBx').val()*100 <=20) 
+{
+  $('.rating-text').html('Failed to Meet standard');
+} else if($('.inputBx').val()*100 <=40) {
+  $('.rating-text').html('Partially Met Standard');
+} else if($('.inputBx').val()*100 <=60) {
+  $('.rating-text').html('Met Standard Submit PV on time Get more Client Appreciation Average time in office - 6 hours ( Please maintain 9 hours in office)');
+} else if($('.inputBx').val()*100 <=80) {
+  $('.rating-text').html('Surpassed Expected standard');
+} else if($('.inputBx').val()*100 <=100) {
+  $('.rating-text').html('Substantially Surpassed standard');
+}
 $('#reload').on('click', function(){
   animate($('#inputVal').val());
 });
+
